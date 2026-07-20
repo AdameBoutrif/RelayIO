a36e7e5e1e466fac378a44bbffc0df390dbc1a1e
Adame <adame.boutrif@outlook.com>
2026-07-20 10:19:50 -0400

    Cleaned up some readidability on return type for get_movies and added prefix and tags on the router for movie.py

--- app_relay/crud/movie.py
+++ app_relay/crud/movie.py
@@ -2,8 +2,12 @@ from sqlalchemy.orm import Session
 
 from app_relay.models import Movie
 
-def get_movies(db: Session):
+def get_movies(db: Session) -> list[Movie]:
     return db.query(Movie).all()
 
 def get_movie(db: Session, movie_id: int) -> Movie | None:
-    return db.query(Movie).filter(Movie.id == movie_id).first()
+    return (
+        db.query(Movie)
+        .filter(Movie.id == movie_id)
+        .first()
+    )
--- app_relay/routers/movie.py
+++ app_relay/routers/movie.py
@@ -5,10 +5,13 @@ from app_relay.database import get_db
 from app_relay.crud.movie import get_movies, get_movie
 from app_relay.schemas import MovieRead
 
-router = APIRouter()
+router = APIRouter(
+    prefix="/movies",
+    tags=["Movies"],
+)
 
 @router.get(
-        "/movies",
+        "",
         response_model=list[MovieRead],
 )
 
@@ -16,14 +19,15 @@ def read_movies(db: Session = Depends(get_db)):
     return get_movies(db)
 
 @router.get(
-        "/movies/{movie_id}",
+        "/{movie_id}",
         response_model=MovieRead,
 )
 
 def read_movie(movie_id: int, db: Session = Depends(get_db)):
-    movie = get_movie(db, movie_id)
+    movie = get_movie(db=db, movie_id=movie_id)
     if movie is None:
         raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Movie not found"
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail="Movie not found"
         )
     return movie
